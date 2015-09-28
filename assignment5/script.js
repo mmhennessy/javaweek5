bfc966a2de1b5dbedf01cb1aeddc9da64287339a
mmhennessy <mhennessy@mcad.edu>
2015-09-27 23:45:53 -0500

    Final Commit
    Changes to assignment 5

--- assignment5/script.js
+++ assignment5/script.js
@@ -1,8 +1,4 @@
-window.prompt("Hi, What is your name?");
-console.log("Okay I'll call you later");
-
-
-
+//This assignment was created in part with help from codes written by fellow students
 var churchillSpeech = {
       'author': 'Churchill',
       'year': 1940,
@@ -23,10 +19,17 @@ var churchillSpeech = {
     },
     speechesArray = [churchillSpeech, ghandiSpeech, demosthenesSpeech];
 
+    var name = window.prompt("Hi, What is your name?");
+    if (name = "") {
+      result = "Hello";
+    } else {
+      result = "Okay I'll just call you User";
+    };
+
 document.getElementById('BtnDonate').addEventListener('click', function(){
 prompt("Who is your favorite author?");
 
-var author = ;
+var author = "" ;
 var result;
 
 switch (author) {
@@ -47,34 +50,49 @@ switch (author) {
 }
 });
 
+var speechyear
+result;
+
+if ("Was the speech written during the common era?") {
+  result = true;
+} else {
+  result = false;
+  console.log("Speech was written before the common era");
+};
+
 document.getElementById('BtnChurchill').addEventListener('click', function(){
   console.log("This speech was written by Churchill in the year 1940, Commmon Era");
 
-  if () {
-    result = "This is the most recent speech on the page";
+  if ("This was the most recent speech") {
+    result = true;
+    console.log("This was the most recent speech on the page");
   } else {
-    result = "This is a more recent speech";
+    result = false;
+    console.log("This is a more recent speech");
   }
 });
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
   console.log("This  speech was written by Ghandi in the year 1942, Common Era");
 
-    if () {
-    result = "This is the most recent speech on the page";
+    if ("This was the most recent speech") {
+      result = true;
+    console.log("This is the most recent speech on the page");
   } else {
-    result = "This is a more recent speech";
+    result = false;
+    console.log("This is a more recent speech");
   }
 });
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
   console.log("This speech was written by Demosthenes in the year 342, Before Commmon Era");
 
-    if () {
-    result = "This is the most recent speech on the page";
+    if ("This was the most recent speech") {
+      result = true;
+      console.log("This is the most recent speech on the page");
   } else {
-    result = "This is a more recent speech";
+    result = false;
+    console.log("This is a more recent speech");
   }
 
 });
-
